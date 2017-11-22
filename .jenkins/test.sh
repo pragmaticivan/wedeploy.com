7a6e6d83a31c3718b8b65cbd2c973c91fb4c24ae
Ivan Santos <pragmaticivan@gmail.com>
2017-11-21 16:06:10 -0800

    Clones console project and run tests

--- .jenkins/test.sh
+++ .jenkins/test.sh
@@ -4,12 +4,14 @@ CURRENT_DIR=$(pwd)
 
 main() {
   setup_tests
+  run_tests
 }
 
 setup_tests() {
   GIT_REPO_TESTS="https://github.com/wedeploy/console-functional-tests.git"
   rm -rf "${CURRENT_DIR}/console-functional-tests"
   echo "Updating images repository"
+  git clone $GIT_REPO_TESTS || true
   cd ${CURRENT_DIR}/console-functional-tests
   git reset --hard
   git pull origin master
